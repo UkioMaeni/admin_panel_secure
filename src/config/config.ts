a3f4b8edc41a5e27f3cc228b8e9689669d4c1d97
ukiomaeni <priz.a47@gmail.com>
2024-07-30 10:36:49 +0500

    ass

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 export const config={
-   // baseHttpUrl:"http://192.168.1.92:3005/api"
-   baseHttpUrl:"http://109.71.247.195:3005/api"
+   //baseHttpUrl:"http://192.168.1.92:3005/api"
+   baseHttpUrl:"http://82.97.245.161:3005/api"
 }
